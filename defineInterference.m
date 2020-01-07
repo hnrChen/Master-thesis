9cf360755268449119979f78fc9825356bb43cc4
hnrChen <54701169+hnrChen@users.noreply.github.com>
2020-01-07 15:01:52 +0100

    define the interference
    define the interference

--- defineInterference.m
+++ defineInterference.m
@@ -1,5 +1,5 @@
-function interferenceParameter = defineInterference(radarParameter,amplitude,SIR,type)
-interferenceParameter.amplitude = amplitude * sqrt(SIR);
+function interferenceParameter = defineInterference(radarParameter,targetParameter,SIR,type)
+interferenceParameter.amplitude = targetParameter.A * sqrt(SIR);
 A = interferenceParameter.amplitude;
 interferenceParameter.type = type;
 %transmitted signal
@@ -8,70 +8,53 @@ interferenceParameter.type = type;
 t_slow = (0 : radarParameter.N_chirp - 1) * radarParameter.T_chirp; 
 % in which sample
 t_fast = (0 : radarParameter.N_sample - 1) * radarParameter.T_sample;
-% phase of the first chirp of the CS radar
 f_cs = radarParameter.f0 + radarParameter.ramp * t_fast;
 f_cs = repmat(f_cs,1,radarParameter.N_chirp);
 % phaseshift because of vr
-fD = -2 * radarParameter.f0 * objectParameter.vr / radarParameter.c0;     % 1 x N_pn
+fD = -2 * radarParameter.f0 * targetParameter.vr / radarParameter.c0;     % 1 x N_pn
 % phaseshift because of r0
-fR = -2 * radarParameter.ramp * objectParameter.r0 / radarParameter.c0;   %scalar
+fR = -2 * radarParameter.ramp * targetParameter.r0 / radarParameter.c0;   %scalar
 % maximal bandwidth for AAF
 B_AAF = radarParameter.ramp * 200 / radarParameter.c0;   % maximal detection distance is 200m
 
 
 %CW radar as the interference
 if type == 1
-    f_cw = 100 * 1e9;   %set the constant frequency for CW radar signal
-    freq_diff = f_cw - f_cs + random();  %set an initial phase for this phase shift
-    % SF = 1;
-    % signal to interference mitgation gain
-    % interferenceParameter.G_sir = radarParameter.B * radarParameter.T_chirp * SF;
-    for i = 1:length(freq_diff)
-        if freq_diff(i) > B_AAF || freq_diff(i) < -1 * B_AAF
-            freq_diff(i) = 0;
-        end
-    end
-    freq_diff = freq_diff + random();  %set an initial phase for this phase shift
-    phase = cumtrapz(freq_diff);  % set a random initial phase
-    reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
+    f_cw = 95 * 1e9;   %set the constant frequency for CW radar signal
+    freq_diff = f_cw - f_cs;  %set an initial phase for this phase shift
+    phase = cumtrapz(2 * pi * freq_diff) + 2 * pi * rand();  % set a random initial phase
+%     reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
     for i = 1 : radarParameter.N_pn
-        interferenceParameter.signal(:,:,i)= A ...
-                           * exp(1j * 2 * pi * reshaped_phase)...
-                           * exp(1j * 2 * pi * fD(ceil(i/radarParameter.N_Rx)) * t_slow)...
-                           * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
-                                                    * radarParameter.P(i, :) * objectParameter.u');
+        interference = A * exp(1j * 2 * pi * phase)...
+                         * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
+                                                    * radarParameter.P(i, :) * targetParameter.u');
+        for j = 1:length(freq_diff)
+             if freq_diff(j) > B_AAF || freq_diff(j) < -1 * B_AAF
+                 interference(j) = 0;
+             end
+        end
+        interferenceParameter.signal(:,:,i) = reshape(interference,[radarParameter.N_sample,radarParameter.N_chirp]);
     end
 
 %FMCW radar as the interference
-%set random chirp rates for different chirps !!!!!!!!!!!
 elseif type == 2
-    % T_fmcw / T_cs = m/n
-    m = 20;
-    n = 3;
-    chirp_ramp_fmcw = 10 * radarParameter.ramp;
-    f0_fmcw = 94 * 1e9;
-    N_sample_fmcw = floor(m * radarParameter.N_sample / n);
-    t = (0:N_sample_fmcw - 1) * radarParameter.T_sample;
-    f_fmcw_first = f0_fmcw + chirp_ramp_fmcw * t;  %frequency of first chirp for FMCW
-    number_chirp_fmcw = floor(radarParameter.N_sample * radarParameter.N_chirp / N_sample_fmcw);
-    f_fmcw = repmat(f_fmcw_first,1,number_chirp_fmcw);
-    len = radarParameter.N_sample * radarParameter.N_chirp - number_chirp_fmcw * N_sample_fmcw;
-    f_fmcw_rest = f_fmcw_first(1:len);
-    f_fmcw = [f_fmcw f_fmcw_rest];
-
-    freq_diff = f_fmcw - f_cs + random();   %set an initial phase for this phase shift
-    for i = 1:length(freq_diff)
-        if freq_diff(i) > B_AAF || freq_diff(i) < -1 * B_AAF   %f_ifmax needs to be define
-            freq_diff(i) = 0;
-        end
-    end
-    phase = cumtrapz(freq_diff_new);   
-    reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
+    t_fmcw = (0 : radarParameter.N_sample * radarParameter.N_chirp - 1) * radarParameter.T_sample;
+    ramp_fmcw = radarParameter.ramp / 100;
+    f_fmcw = radarParameter.f0 + ramp_fmcw * t_fmcw;
+    freq_diff = f_fmcw - f_cs;   %set an initial phase for this phase shift
+    
+    phase = cumtrapz(2 * pi * freq_diff) + 2 * pi * rand();    %set an initial phase for this phase shift
+%     reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
     for i = 1 : radarParameter.N_pn
-       interferenceParameter.signal(:,:,i) = A ...
-                           * exp(1j * 2 * pi * reshaped_phase)...
-                           * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
-                                                    * radarParameter.P(i, :) * objectParameter.u');
+        interference = A * exp(1j * 2 * pi * phase)...
+                         * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
+                                                    * radarParameter.P(i, :) * targetParameter.u');
+        for j = 1:length(freq_diff)
+             if freq_diff(j) > B_AAF || freq_diff(j) < -1 * B_AAF
+                 interference(j) = 0;
+             end
+        end
+        interferenceParameter.signal(:,:,i) = reshape(interference,[radarParameter.N_sample,radarParameter.N_chirp]);
     end
     
     
@@ -79,50 +62,60 @@ elseif type == 2
 else
     %CS interference with same chirp rate and duration
     if type == 3
-        freq_diff = ones(1,radarParameter.N_sample * radarParameter.N_chirp) * random(); % just a different initial phase different:
-        for i = 1:length(freq_diff)
-            if freq_diff(i) > B_AAF || freq_diff(i) < -1 * B_AAF   %f_ifmax needs to be define
-                freq_diff(i) = 0;
-            end
-        end
-        phase = cumtrapz(freq_diff);
-        reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
+        freq_diff = ones(1,radarParameter.N_sample * radarParameter.N_chirp) * 2 * B_AAF; % just a different initial phase different:
+        phase = cumtrapz(2 * pi * freq_diff) + 2 * pi * rand();
         for i = 1 : radarParameter.N_pn
-            interferenceParameter.signal(:,:,i)= A ...
-                           * exp(1j * 2 * pi * reshaped_phase)...
-                           * exp(1j * 2 * pi * fD(ceil(i/radarParameter.N_Rx)) * t_slow)...
-                           * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
-                                                    * radarParameter.P(i, :) * objectParameter.u');
-        end
-        
-        
+            interference = A * exp(1j * 2 * pi * phase)...
+                            * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
+                                                    * radarParameter.P(i, :) * targetParameter.u');
+            interferenceParameter.signal(:,:,i) = reshape(interference,[radarParameter.N_sample,radarParameter.N_chirp]);
+        end     
     %CS interference with same chirp duration but different rate
     elseif type == 4
         f0_inter = radarParameter.f0;  % set a different carrier frequency or not?
-        ramp_inter = radarParameter.ramp + random();
+        ramp_inter = radarParameter.ramp * (rand() + 1);
         f_cs_inter = f0_inter + ramp_inter * t_fast;  % frequency of the first chirp of the CS interference
         f_cs_inter = repmat(f_cs_inter,1,radarParameter.N_chirp);
-
-        freq_diff = f_cs_inter - f_cs + random();  % set a random initial phase for this phase difference
-        for i = 1:length(freq_diff)
-            if freq_diff(i) > B_AAF || freq_diff(i) < -1 * B_AAF   
-                freq_diff(i) = 0;
-            end
-        end
-        phase = cumtrapz(freq_diff);
-        reshaped_phase = reshape(phase,[radarParameter.N_sample,radarParameter.N_chirp]);
+        freq_diff = f_cs_inter - f_cs;  % set a random initial phase for this phase difference
+        phase = cumtrapz(2 * pi * freq_diff) + 2 * pi * rand();
         for i = 1 : radarParameter.N_pn
-            interferenceParameter.signal(:,:,i)= A ...
-                           * exp(1j * 2 * pi * reshaped_phase)...
-                           * exp(1j * 2 * pi * fD(ceil(i/radarParameter.N_Rx)) * t_slow)...
-                           * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
-                                                    * radarParameter.P(i, :) * objectParameter.u');
+            interference = A * exp(1j * 2 * pi * phase)...
+                         * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
+                                                    * radarParameter.P(i, :) * targetParameter.u');
+            for j = 1:length(freq_diff)
+                if freq_diff(j) > B_AAF || freq_diff(j) < -1 * B_AAF
+                    interference(j) = 0;
+                end
+            end
+            interferenceParameter.signal(:,:,i) = reshape(interference,[radarParameter.N_sample,radarParameter.N_chirp]);
         end
         
     %CS interference with same chirp rate but different duration
     elseif type == 5
         ramp_inter = radarParameter.ramp;
+        % case 1: N_chirp > m %case 2: N_chirp < m      N_chirp = 160
+        m = 171; n = 200;  % T_chirp_interference / T_chirp = m / n
+        N_chirp_inter = floor(n * radarParameter.N_chirp / m) + 1;
+        t_cs = (0 : floor(radarParameter.N_sample * m / n) - 1) * radarParameter.T_sample;
+        f_cs_inter = radarParameter.f0 + ramp_inter * t_cs;
+        f_cs_inter = repmat(f_cs_inter,1,N_chirp_inter);
+        f_cs_inter = f_cs_inter(1:length(f_cs));
         
+        freq_diff = f_cs_inter - f_cs;  
+        phase = cumtrapz(2 * pi * freq_diff) + 2 * pi * rand();
+        for i = 1 : radarParameter.N_pn
+            interference = A * exp(1j * 2 * pi * phase)...
+                         * exp(-1j * 2 * pi * radarParameter.f0(ceil(i/radarParameter.N_Rx)) / radarParameter.c0...
+                                                    * radarParameter.P(i, :) * targetParameter.u');
+            for j = 1:length(freq_diff)
+                if freq_diff(j) > B_AAF || freq_diff(j) < -1 * B_AAF
+                    interference(j) = 0;
+                end
+            end
+            interferenceParameter.signal(:,:,i) = reshape(interference,[radarParameter.N_sample,radarParameter.N_chirp]);
+        end
+        
+
         
         
     %CS interference with different chirp duration and rate   
@@ -133,5 +126,8 @@ else
     end
 end
     
+    
+    
+
     
     
