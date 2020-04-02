eb1c07762edbfc0dbf2761d32621314450688585
hnrChen <54701169+hnrChen@users.noreply.github.com>
2020-04-02 13:51:04 +0200

    Update energyDetector.m

--- energyDetector.m
+++ energyDetector.m
@@ -1,10 +1,13 @@
-function [reconstructedSignal, interferedIndex] = energyDetector(interferedSignal,channel)
+function [reconstructedSignal, interferedIndex] = energyDetector(radarParameter,interferedSignal,channel)
 signal  = interferedSignal(:, :, channel);
 [m, n] = size(signal);
 signal = reshape(signal,[1,m * n]);
 squareSignal = abs(signal) .^ 2;
+% maximal bandwidth for AAF
+B_AAF = radarParameter.ramp * 200 / radarParameter.c0;   % maximal detection distance is 200m
 % M is the expected length of the interfered signal
-M = L_chirp;
+% M = floor(2 * B_AAF * radarParameter.N_sample / radarParameter.B);
+M = floor(B_AAF * radarParameter.N_sample / radarParameter.B);
 % calculate the average enengy of the interfered signal
 average_energy = mean(squareSignal);
 % set a certain threshold for energy detector
@@ -16,17 +19,18 @@ for i = 2 : length(signal) - M
     energy  = mean(squareSignal(i : i + M - 1));
     if energy >= threshold
         % remove the interfered samples
-        signal(i : i + M - 1 ) = 0;
+%         signal(i : i + M - 1 ) = 0;
         interferedIndex(i : i + M - 1) = 1;
         % reconstruct the echo signal of the interfered parts
-        for j = i : i + M - 1
-            uninterferedSignal = signal(1:j-1);
-            weights = rlsfilter(uninterferedSignal,2);
-            inverted_weights = fliplr(weights(2,:));
-            signal(j) = sum(inverted_weights .* uninterferedSignal);
-        end
+%         for j = i : i + M - 1
+%             uninterferedSignal = signal(1:j-1);
+%             weights = lmsfilter(uninterferedSignal);
+%             inverted_weights = fliplr(weights);
+%             signal(j) = sum(inverted_weights .* uninterferedSignal);
+%         end
     end
-    reconstructedSignal = signal;
+    reconstructedSignal = reshape(signal,[m,n]);
+    interferedIndex = reshape(interferedIndex,[m,n]);
 end
 
 
